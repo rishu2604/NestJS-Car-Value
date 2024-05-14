794f652c51417b761d0371a3fad7086da9ad0838
Rishu Anand <anand.rishu310@gmail.com>
2024-05-14 13:25:22 +0530

    Explaining what is whitelist in validationPipe

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true
     })
+    // The purpose of this property is to make sure that incoming requests don't have extraneous properties in the body that we are not expecting.
+    // So for example, inside of our createUserDTO, we had said that only the email and password properties should exist by adding in whitelist of true right there.
+    // That's going to make sure that any additional properties that we send along with the request will be stripped out for us automatically.
   )
   await app.listen(3000);
 }
