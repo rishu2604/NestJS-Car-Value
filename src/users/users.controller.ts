62443e5f82db45d22e26f292afa547f032d1e0d5
Rishu Anand <anand.rishu310@gmail.com>
2024-05-15 15:57:11 +0530

    Minor refactors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -12,7 +12,11 @@ export class UsersController {
     }
 
     @Get(':/id')
-    findUser(@Param('id') id: string){
+    async findUser(@Param('id') id: string){
+        const user = await this.userService.findOne(parseInt(id));
+        if(!user){
+            throw new NotFoundException('User not found');
+        }
         return this.userService.findOne(parseInt(id));
     }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -38,7 +38,7 @@ export class UsersService {
         const user = await this.repo.findOneBy({id});
 
         if(!user){
-            throw new Error('User not found');
+            throw new NotFoundException('User not found');
         }
 
         Object.assign(user, attributes); // Overwrites the user object with the attributes object.
@@ -48,7 +48,7 @@ export class UsersService {
     async remove(id: number){
         const user = await this.repo.findOneBy({id});
         if(!user){
-            throw new Error('User not found');
+            throw new NotFoundException('User not found');
         }
         return this.repo.remove(user); // 👈 Removes the record from the DB. Hooks can be executed with remove method
     }
