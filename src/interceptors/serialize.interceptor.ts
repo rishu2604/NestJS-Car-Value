c35d2d19f86c4d6206fbbbb198469a4cde23ba00
Rishu Anand <anand.rishu310@gmail.com>
2024-05-16 13:31:33 +0530

    Fixed a minor bug in the serialize.interceptor.ts

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -14,7 +14,7 @@ export class SerializeInterceptor implements NestInterceptor{
             map((data: any)=>{
                 // Run something before the response is sent out
                 // console.log('I am running before the response is sent out', data);
-                return plainToClass(UserDto, data{
+                return plainToClass(UserDto, data, {
                     excludeExtraneousValues: true
                 })
             })
